50126490af6d5c99e403955a7d74f3ef44c6fcda
Kip Raske <kip.raske@gmail.com>
2014-11-19 16:45:59 -0600

    Added simple ws server for testing.  Next is to update the client code to create a real connection.

--- server/ws/ws-server.js
+++ server/ws/ws-server.js
@@ -1,1 +1,24 @@
-console.log("ws server loaded");
+
+var router = require("./router");
+
+var wsPort = 8080;
+
+var WebSocketServer = require("ws").Server;
+var wss = new WebSocketServer({port: wsPort});
+
+wss.on("connection", function(ws) {
+   
+    console.log("ws server listening on port %s", wsPort);
+    
+    ws.on("message"), function(msg){
+       
+       // TODO route incoming message
+       
+       console.log("recieved %s", msg);
+   } 
+   
+   
+   //register outgoing messages
+   
+   
+});
