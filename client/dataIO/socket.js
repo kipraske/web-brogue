fe2509ed8166b3e5a65821e30fabafa2aaa5093d
Kip Raske <kip.raske@gmail.com>
2014-11-19 17:03:50 -0600

    Client updated to talk to web server socket

--- client/dataIO/socket.js
+++ client/dataIO/socket.js
@@ -1,46 +1,18 @@
 define(['dataIO/router'], function(router){
-   //fake socket so I can hook it up
-    var socket = {
-       send : function (msg){
-           console.log ("test message sent: ", msg);
-       },
-       close : function (){
-           console.log ("test socket closing...");
-           this.onclose();
-       },
-       onopen : function () {
-           console.log("test socket open");
-       },
-       onerror : function () {
-           console.error("test socket error thrown");
-       },
-       onclose : function () {
-           console.log("test socket closed");
-       },
-       onmessage : function (evt) {
-           
-           // deflate message
-           
-           // parse JSON
-           
-           // split datatype from payload
-           
-           var payload = { json : "testdata"};
-           var msgType = "test";
-           
-           // TODO: it seems the stone soup code contains a message queue which is handled when the script is ready for the next message
-           // That would be a nice thing to do  here too
-           
-           // TODO : I think the router should be able to handle each of these steps
-           // router.prepareData -> msg.dataType and msg.data etc
-           // router.enqueueMessage(msg)
-           // then in router router.dispatch(msg)-> calls the appropriate handler when system resources are good
-           
-           router.dispatch(msgType, payload);
-       }
-   }; 
-   
-   socket.onopen();
+    
+    // TODO: make this host URL not hardcoded
+    
+    var socket = new WebSocket("ws://localhost:8080");
+    
+    socket.onopen = function(){
+        console.log("test socket open");
+        
+        socket.send("test message to server");
+    };
+    
+    socket.onmessage = function(event){
+        console.log("message recieved " + event.data);
+    };
    
    return socket;
 });
--- server/ws/ws-server.js
+++ server/ws/ws-server.js
@@ -10,12 +10,12 @@ wss.on("connection", function(ws) {
    
     console.log("ws server listening on port %s", wsPort);
     
-    ws.on("message"), function(msg){
+    ws.on("message", function(msg){
        
        // TODO route incoming message
        
        console.log("recieved %s", msg);
-   } 
+    });
    
    
    //register outgoing messages
