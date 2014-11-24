db1db849e2bdaaf0dd40600608a2fcf9b324d0e5
Kip Raske <kip.raske@gmail.com>
2014-11-23 20:53:13 -0600

    Updated the mock brogue process to send back the worst case scenario of packets - 3400 of them.

--- server/tests/mock-brogue-process.js
+++ server/tests/mock-brogue-process.js
@@ -1,30 +1,33 @@
+function writeOutPutBuffer(counter) {
 
-process.stdin.on("readable", function(){
-    
-    var chunk = process.stdin.read();
-    
-    if (chunk != null){
-        // do something with input
+    if (counter > 3400) {
+        return;
     }
-    
+
     var ROW_NUMBER = 34;
     var COL_NUMBER = 100;
-    
     var RETURN_SIZE = 9;
+
     var buf = new Buffer(RETURN_SIZE);
-    
-    var rowRand = Math.floor(Math.random()* ROW_NUMBER);
-    buf.writeUInt8(rowRand, 0);
-   
+
     var colRand = Math.floor(Math.random() * COL_NUMBER);
-    buf.writeUInt8(colRand, 1);
-    
-    for (var i = 2; i < RETURN_SIZE; i++){
-        var rand = Math.floor(Math.random()*255);
+    buf.writeUInt8(colRand, 0);
+    var rowRand = Math.floor(Math.random() * ROW_NUMBER);
+    buf.writeUInt8(rowRand, 1);
+    for (var i = 2; i < RETURN_SIZE; i++) {
+        var rand = Math.floor(Math.random() * 255);
         buf.writeUInt8(rand, i);
     }
 
-    process.stdout.write(buf);
+    process.stdout.write(buf, function(err) {
+        if (err) {
+            throw err;
+        }
+        writeOutPutBuffer(++counter);
+    });
+}
+
+process.stdin.on("readable", function() {
+    writeOutPutBuffer(0);
+    process.stdin.read();
 });
-
-
