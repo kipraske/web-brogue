0e70b8d0ff66b74febb9cf8cbdda84574603ebc1
Kristofer Raske <kristofer.raske@wustl.edu>
2014-12-09 12:06:06 -0600

    Remove testing console logs and add some comments to help understand this data/remainder nonsense that I created.

--- server/controllers/brogue-controller.js
+++ server/controllers/brogue-controller.js
@@ -51,19 +51,14 @@ function BrogueController(ws, user) {
 
     this.attachChildEvents = function() {
 
-        console.log("expected total bytes: 30600");
-
         self.brogueChild.stdout.on('data', function(data) {
             
-            console.log("data event started");
-            
+            // Ensure that we send out data in chunks divisible by CELL_MESSAGE_SIZE and save any left over for the next data event
             var dataLength = data.length;
             var remainderLength = self.dataRemainder.length;
-            var numberOfCellsToSend = (dataLength + remainderLength) / CELL_MESSAGE_SIZE | 0;  // |0 is still 2x faster than Math.floor, so we use that here though it is not so easy to read.
+            var numberOfCellsToSend = (dataLength + remainderLength) / CELL_MESSAGE_SIZE | 0;  // |0 is still 2x faster than Math.floor or parseInt
             var sizeOfCellsToSend = numberOfCellsToSend * CELL_MESSAGE_SIZE;
             var newReminderLength = dataLength + remainderLength - sizeOfCellsToSend;
-            
-            console.log("dataLength: %s\n remainderLength: %s\n numberOfCells: %s\n sizeOfCells: %s\n newRemainder: %s\n", dataLength, remainderLength,numberOfCellsToSend, sizeOfCellsToSend, newReminderLength);
             
             //fill the data to send 
             self.dataAccumulator = new Buffer(sizeOfCellsToSend);
@@ -76,16 +71,6 @@ function BrogueController(ws, user) {
             
             ws.send(self.dataAccumulator, {binary: true});     
         });
-        
-        /*
-        self.brogueChild.stdout.on('readable', function() { 
-            var chunk = self.brogueChild.stdout.read(CELL_MESSAGE_SIZE);
-            while (chunk !== null) {
-                ws.send(chunk, {binary: true});
-                chunk = self.brogueChild.stdout.read(CELL_MESSAGE_SIZE);
-            }         
-        });
-        */
     };
 }
 
