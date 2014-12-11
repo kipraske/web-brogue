64b5609dc5f0f8da94370ce8b7c982800c6a553f
Kip Raske <kip.raske@gmail.com>
2014-12-10 20:14:45 -0600

    Adding some files that outline the input strategy that I am employing.

---
+++ client/dataIO/prepare-keypress.js
@@ -0,0 +1,9 @@
+// TODO - create an arraybuffer to send to the server and hook that into the console
+// On the node server we will check the bounds of each element (don't want anything weird on the c side) and then pass it in stdin
+
+// Here will be enums and maps and stuff for minimal processing on the c side
+
+// See PlatformCode/webplaform.c for how these are ultimately used
+// See BrogueCode/rogue.h for all event definitions
+var KEYPRESS_EVENT_CHAR = 0;
+
---
+++ client/dataIO/prepare-mouse.js
@@ -0,0 +1,8 @@
+// TODO - see note on prepare-keypress cuz we are doing the same thing here
+
+var MOUSE_UP_EVENT_CHAR = 1;
+var MOUSE_DOWN_EVENT_CHAR = 2;
+var RIGHT_MOUSE_DOWN_EVENT_CHAR = 3;
+var RIGHT_MOUSE_UP_EVENT_CHAR = 4;
+var MOUSE_HOVER_EVENT_CHAR = 5;
+
