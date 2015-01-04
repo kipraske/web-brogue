8d95fbb1a2d9ac029c19d85bf026d36affbe4ab6
Kip Raske <kip.raske@gmail.com>
2015-01-03 19:41:07 -0600

    Adding template for the user rollup list - probably not going to do much more with this at the moment

---
+++ client/models/current-games.js
@@ -0,0 +1,18 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone'
+], function($, _, Backbone) {
+
+    var CurrentGames = Backbone.Model.extend({
+        defaults: {
+            // TODO - yeah model
+        }
+    });
+
+    return CurrentGames;
+
+});
+
+
+
--- client/views/current-games-view.js
+++ client/views/current-games-view.js
@@ -1,7 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+define([
+    "jquery",
+    "underscore",
+    "backbone",
+    "models/current-games"
+], function ($, _, Backbone) {
 
+    var CurrentGamesView = Backbone.View.extend({
+        el: "#current-games",
+        model: new CurrentGamesModel(),
+        
+        initialize: function(){
+            
+        },
+        
+        render: function(){
+            
+        },
+        
+        updateUserData: function(){
+            
+        }
+    });
+    
+    return CurrentGamesView;
 
+});
