8ff548432744ba238b0ca8a9cb6b1ca80ddf44a6
Kip Raske <kip.raske@gmail.com>
2015-01-06 12:11:39 -0600

    Oh should probably hide the lobby when you start playing

--- client/views/play-view.js
+++ client/views/play-view.js
@@ -17,6 +17,7 @@ define([
             
             send("brogue", "start");
             $("#console").removeClass("inactive");
+            $("#lobby").addClass("inactive");
         }
     });
     
