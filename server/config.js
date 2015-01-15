c8fcf3761ad46e8c46d4cafb0c6002dee0c1d415
kipraske <kip.raske@gmail.com>
2015-01-15 16:05:34 -0600

    Need to update the broguepath for our posix executable.  It is now fully running on posix environment

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 var config = {
     CLIENT_DIR : path.normalize(__dirname + "/../client/"),
-    BROGUE_PATH : path.normalize(__dirname + "/../brogue/brogue.exe"),
+    BROGUE_PATH : path.normalize(__dirname + "/../brogue/bin/brogue"),
     GAME_DATA_DIR : path.normalize(__dirname + "/../game-data/"),
     //BROGUE_PATH : path.normalize(__dirname + "/tests/mock-brogue-process.js")
     db : {
