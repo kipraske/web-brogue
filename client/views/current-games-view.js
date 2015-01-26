659156bb4388f7499236707ed52f11b07000982a
kipraske <kip.raske@gmail.com>
2015-01-26 05:48:41 -0600

    *Partial* - starting the front-end lobby view.

--- client/views/current-games-view.js
+++ client/views/current-games-view.js
@@ -2,22 +2,34 @@ define([
     "jquery",
     "underscore",
     "backbone",
-    "models/current-games-row"
-], function ($, _, Backbone, CurrentGamesRowModel) {
+    "views/current-games-row-view",
+    "models/current-games-row",
+], function ($, _, Backbone, CurrentGamesRowView, CurrentGamesRowModel) {
+
+    var rowViewCollection = {};
 
     var CurrentGamesView = Backbone.View.extend({
         el: "#current-games",
         
-        initialize: function(){
-
+        render: function(){ 
+            console.log(rowCollection);
+            // TODO - if the rowView render is NOT called remove it from the collection hrm... can one self-destruct in js?
         },
         
-        render: function(){
+        updateUserData: function(data){
+            for (var userName in data){              
+                var update = data[userName];
+                
+                if (!rowCollection[userName]) {
+                    var rowModel = new CurrentGamesRowModel(update);
+                    rowCollection[userName] = new CurrentGamesRowView(rowModel);          
+                }
+                else {
+                    rowCollection[userName].model.set(update);
+                }
+            }
             
-        },
-        
-        updateUserData: function(){
-            
+            this.render();
         }
     });
     
