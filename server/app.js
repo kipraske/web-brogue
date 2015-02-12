67136b442386c6756ee1d97759187a13c04d7f4b
kipraske <kip.raske@gmail.com>
2015-02-12 05:33:11 -0600

    Now clean up app.js a bit - I am very happy with that.  It is much easier to read.

--- server/app.js
+++ server/app.js
@@ -6,19 +6,18 @@ var app = express();
 var mongoose = require("mongoose");
 mongoose.connect(config.db.url);
 
-// may want to do something similar for session configuration.  First things first - websocket
+// TODO - may want to do something similar for session configuration.  First things first - websocket
 
 // Http Server Configuration
 app.use(express.static(config.CLIENT_DIR));
 
 //routes
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(config.CLIENT_DIR + "/index.html");
 });
 
-httpServer = app.listen(80, function(){
-    
-var port = httpServer.address().port;
+httpServer = app.listen(80, function () {
+    var port = httpServer.address().port;
     console.log("Server listening on port %s", port);
 });
 
@@ -26,16 +25,11 @@ var port = httpServer.address().port;
 var WebSocketServer = require("ws").Server;
 var wss = new WebSocketServer({server: httpServer});
 
-var ErrorController = require("./controllers/error-controller");
-var BrogueController = require("./controllers/brogue-controller");
-var AuthController = require("./controllers/authentication-controller");
-var LobbyController = require("./controllers/lobby-controller");
-
 var controllerFactory = require("./controllers/controller-factory")
 var controllerCleanUp = require("./controllers/cleanup-controllers.js");
 var Router = require("./controllers/router");
 
-wss.on("connection", function(ws) {
+wss.on("connection", function (ws) {
 
     var controllers = controllerFactory(ws, [
         "error",
@@ -43,14 +37,14 @@ wss.on("connection", function(ws) {
         "authentication",
         "brogue"
     ]);
-    
+
     var router = new Router(controllers);
-    
-    ws.on("message", function(message){
-       router.route(message);
+
+    ws.on("message", function (message) {
+        router.route(message);
     });
-    
-    ws.on("close", function(code, message){
+
+    ws.on("close", function (code, message) {
         controllerCleanUp(controllers);
     });
 });
