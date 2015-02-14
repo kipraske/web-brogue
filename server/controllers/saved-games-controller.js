c7e72c358de41c32da215cf3205b4ce47aa24a47
kipraske <kip.raske@gmail.com>
2015-02-14 15:47:46 -0600

    saved games controller logic complete.  Though these two loops can probably be combined now.

--- server/controllers/saved-games-controller.js
+++ server/controllers/saved-games-controller.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 var Controller = require('./controller-base');
 
 var fs = require('fs');
+var path = require('path');
 
 // Controller for displaying saved games for loading
 
@@ -23,18 +24,39 @@ _.extend(SavedGamesController.prototype, {
             fs.readdir(config.GAME_DATA_DIR + currentUserName, function (err, files) {
                 if (err) {
                     self.controllers.error.send(JSON.stringify(err));
+                    return;
                 }
+
+                var savedGamesNames = [];
 
                 for (var i = 0, numberOfFiles = files.length; i < numberOfFiles; i++) {
                     var fileName = files[i];
                     var brogueSaveRegex = /\.broguesave$/i;
 
                     if (brogueSaveRegex.test(fileName)) {
-
+                        savedGamesNames.push(fileName);
                     }
-
                 }
 
+                if (savedGamesNames.length === 0){
+                    return;
+                }
+
+                var savedGameReturnData = []
+                
+                for (var i = 0, numberOfSavedGames = savedGamesNames.length; i < numberOfSavedGames; i++){
+                    var fileName = savedGamesNames[i];
+                    var filePath = path.normalize(config.GAME_DATA_DIR + currentUserName + '/' + fileName)
+                    var fileStats = fs.stat(filePath);
+                    
+                    savedGameReturnData.push({
+                        fileName : fileName,
+                        modified : fileStats.mtime
+                    });
+                }
+
+                this.sendMessage("saved games", savedGameReturnData);
+                
             });
         }
     }
