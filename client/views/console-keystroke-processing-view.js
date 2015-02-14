5646f0d50d95bc9d856016266eca41f76e2b594e
kipraske <kip.raske@gmail.com>
2015-02-13 19:42:47 -0600

    These keycodes are working for the arrows great.  Looks like we may want to use the capital letters when shift is held though.

--- client/views/console-keystroke-processing-view.js
+++ client/views/console-keystroke-processing-view.js
@@ -22,6 +22,11 @@ define([
         // keydown fires before input is calculated
         keydownHandler : function(event){            
             var keyCode = event.keyCode;
+            var ctrlKey = event.ctrlKey;
+            var shiftKey = event.shiftKey;
+            
+            this.model.set("ctrlKeyHeld", ctrlKey);
+            this.model.set("shiftKeyHeld", shiftKey);
             
             // Ignore key events for lone modifiers
             if (keyCode === 16 ||       // Shift
@@ -30,8 +35,45 @@ define([
                 return;
             }
             
-            this.model.set("ctrlKeyHeld", event.ctrlKey);
-            this.model.set("shiftKeyHeld", event.shiftKey);
+            var returnCode;
+            
+            switch (keyCode){
+                case 13: //enter (\n)
+                    returnCode = 13;
+                    break;
+                case 27: //esc
+                    returnCode = 27;
+                    break;
+                case 33: //page up (9 or u)
+                    returnCode = 117;
+                    break;
+                case 34: //page_down (3 or n)
+                    returnCode = 110;
+                    break;
+                case 35: //end (1 or b)
+                    returnCode = 98;
+                    break;
+                case 36: //home (7 or y)
+                    returnCode = 121;
+                    break;
+                case 37: //left-arrow (4 or h)
+                    returnCode = 104;
+                    break;
+                case 38: //up-arrow(8 or k)
+                    returnCode = 107;
+                    break;
+                case 39: //right-arrow(6 or l)
+                    returnCode = 108;
+                    break;
+                case 40: //down-arrow(2 or j)
+                    returnCode = 106;
+                    break;
+            }
+
+            if (returnCode){
+                sendKeypressEvent(KEYPRESS_EVENT_CHAR, returnCode, ctrlKey, shiftKey);
+            }
+
         },
         
         // input fires after input is calculated
