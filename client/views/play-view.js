6020ccf8e4829676e5775d0052e7426d4d41f314
kipraske <kip.raske@gmail.com>
2015-02-16 08:58:32 -0600

    Adding links to the play view so we can see our saved games rollup.

--- client/views/play-view.js
+++ client/views/play-view.js
@@ -10,13 +10,37 @@ define([
         el: "#play",
         
         events: {
-            "click #brogue-1-7-4" : "startBrogue"
+            "click #start-brogue" : "startBrogue",
+            "click #start-brogue-seed" : "startBrogueSeed",
+            "click #show-saved-games" : "showSavedGames",
+            "click #show-current-games" : "showCurrentGames",
         },
         startBrogue: function(event){
             event.preventDefault();
             
             send("brogue", "start");
             this.goToConsole();
+        },
+        
+        startBrogueSeed: function(event){
+            event.preventDefault();
+            
+            // TODO - show seed form
+            console.log("Here is when we show the view")
+        },
+        
+        showSavedGames : function(event){
+            event.preventDefault();
+            
+            send('savedGames', 'getBrogueSaves');
+            $('#current-games').addClass('inactive');
+            $('#saved-games').removeClass('inactive');
+        },
+        
+        showCurrentGames : function(event){
+            event.preventDefault();
+            $('#saved-games').addClass('inactive');
+            $('#current-games').removeClass('inactive');   
         },
         
         goToConsole : function(){
