dbe6e19fb9826ba0d528a3a2315dd7736e1f530e
kipraske <kip.raske@gmail.com>
2015-02-17 13:16:46 -0600

    Lower the lobby update interval.  I am getting rid of my tests folder since really I am not using it.  Probably should make tests for each controller hrm...

--- server/config.js
+++ server/config.js
@@ -6,11 +6,10 @@ var config = {
     CLIENT_DIR : path.normalize(__dirname + "/../client/"),
     BROGUE_PATH : path.normalize(__dirname + "/../brogue/bin/brogue"),
     GAME_DATA_DIR : path.normalize(__dirname + "/../game-data/"),
-    //BROGUE_PATH : path.normalize(__dirname + "/tests/mock-brogue-process.js")
     db : {
         url : "mongodb://localhost/brogue"
     },
-    LOBBY_UPDATE_INTERVAL : 5000
+    LOBBY_UPDATE_INTERVAL : 1000
 };
 
 module.exports = config;
