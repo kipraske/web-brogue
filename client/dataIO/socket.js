faf337384df4da1cecb358fc99eae24a6dc9a520
kipraske <kip.raske@gmail.com>
2015-02-17 14:04:31 -0600

    I suppose I don't realy need an onopen event for the websocket.  Technically we don't want people trying to login before the handshake is complete, but that seems unlikely.

--- client/dataIO/socket.js
+++ client/dataIO/socket.js
@@ -6,12 +6,6 @@ define(['dataIO/router'], function(router){
     var socket = new WebSocket(socketUrl);
     socket.binaryType = "arraybuffer";
     
-    // TODO : set up onerror, onclose all that stuff
-    
-    socket.onopen = function(){
-        console.log("Web Socket Connection Open");
-    };
-    
     socket.onmessage = function(event){
         router.route(event.data);
     };
