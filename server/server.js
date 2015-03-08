6615617f61c5c20350c35161eebb71d22bca462c
Kristofer Raske <kip.raske@gmail.com>
2015-03-08 21:34:35 +0000

    Adding domains to our http server and websocket server.  This really is not the best way to go about this, but it is not super bad either.
    The long story is that the ws library that I am using is emmiting errors that are not being listened to... but I don't really see any problems from ignoring them.  So in this case if the http server has an error, I think we are good to crash the server.  But the websocket may emit errors that we can ignore BUT we currently can't listen to.
    
    This is a contrived way to listen to the wss object for errors.

--- server/server.js
+++ server/server.js
@@ -1,72 +1,88 @@
 // Main entry point in server side code.  Sets up http server and websocket server and routes
 
+var domain = require("domain");
+
 var config = require("./config");
 var util = require("util");
-
-var express = require("express");
-var app = express();
 
 var mongoose = require("mongoose");
 mongoose.connect(config.db.url);
 
 // Http Server Configuration
-app.use(express.static(config.path.CLIENT_DIR));
+httpServerDomain = domain.create();
+httpServerDomain.on("error", function (err) {
+    util.log("Uncaught exception emmitted internally from Http Server. Process Terminating.");
+    console.log(err.stack);
+    process.exit(1);
+});
+
+httpServerDomain.run(function () {
+
+    var express = require("express");
+    var app = express();
+
+    app.use(express.static(config.path.CLIENT_DIR));
 
 // TODO - configure cookie session, the value is saved in the db, we just need to hook it up
 
 //routes
-app.get("/", function (req, res) {
-    res.sendFile(config.path.CLIENT_DIR + "/index.html");
-});
+    app.get("/", function (req, res) {
+        res.sendFile(config.path.CLIENT_DIR + "/index.html");
+    });
 
-var httpServer = app.listen(config.port.HTTP, function() {
-    console.log("http server listening on port %s", config.port.HTTP);
-});
+    var httpServer = app.listen(config.port.HTTP, function () {
+        console.log("http server listening on port %s", config.port.HTTP);
+    });
 
-httpServer.on("clientError", function(err, socket){
-    util.log("Error from client socket in http server: " + err);
+    httpServer.on("clientError", function (err, socket) {
+        util.log("Error from client socket in http server: " + err);
+    });
 });
 
 // Web Socket Server
-var WebSocketServer = require("ws").Server;
-var wss = new WebSocketServer({port:config.port.WS}, function(){
-    console.log("ws server listening on port %s", config.port.WS);
+websocketServerDomain = domain.create();
+websocketServerDomain.on("error", function (err) {
+    util.log("Unhandled Exception emitted from web socket server.");
+    console.log(err);
+    process.exit(1);
 });
 
-var controllerFactory = require("./controllers/controller-factory")
-var controllerCleanUp = require("./controllers/cleanup-controllers.js");
-var Router = require("./controllers/router");
-
-wss.on("connection", function (ws) {
-
-    var controllers = controllerFactory(ws, [
-        "error",
-        "lobby",
-        "authentication",
-        "saved-games",
-        "brogue"
-    ]);
-
-    var router = new Router(controllers);
-
-    ws.on("message", function (message) {
-        router.route(message);
+websocketServerDomain.run(function () {
+    var WebSocketServer = require("ws").Server;
+    var wss = new WebSocketServer({port: config.port.WS}, function () {
+        console.log("ws server listening on port %s", config.port.WS);
     });
 
-    ws.on("close", function (code, message) {
-        controllerCleanUp(controllers);
-    });
-    
-    ws.on("error", function(err){
-        util.log('Error emitted internally from web socket instance: ' + err); 
-    });
-});
+    var controllerFactory = require("./controllers/controller-factory")
+    var controllerCleanUp = require("./controllers/cleanup-controllers.js");
+    var Router = require("./controllers/router");
 
-wss.on("error", function(err){
-   util.log('Error emitted internally from web socket server: ' + err); 
-});
+    wss.on("connection", function (ws) {
 
-// Process Handlers
-process.stdout.on('error', function(err){
-   util.log('Error when writing to process stdout: ' + err); 
+        var controllers = controllerFactory(ws, [
+            "error",
+            "lobby",
+            "authentication",
+            "saved-games",
+            "brogue"
+        ]);
+
+        var router = new Router(controllers);
+
+        ws.on("message", function (message) {
+            router.route(message);
+        });
+
+        ws.on("close", function (code, message) {
+            controllerCleanUp(controllers);
+        });
+
+        ws.on("error", function (err) {
+            util.log('Error emitted internally from web socket instance: ' + err);
+        });
+    });
+
+    wss.on("error", function (err) {
+        util.log('Error emitted internally from web socket server: ' + err);
+    });
 });
