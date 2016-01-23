c74e0a9c7286697fc73c634f486a6360a427c72f
flend <flend@debian.debian>
2016-01-23 23:26:44 +0000

    Updating site news

--- client/models/site-news-collection.js
+++ client/models/site-news-collection.js
@@ -13,16 +13,23 @@ define([
         addDefaultData: function() {
             var note = new SiteNews();
             note.set({
-                "date": new Date(2016, 1, 12, 0, 0, 0),
+                "date": new Date(2016, 0, 23, 0, 0, 0),
+                "description": "Anonymous users now have names and can watch recordings"
+            });
+            this.add(note);
+            var note = new SiteNews();
+            note.set({
+                "date": new Date(2016, 0, 12, 0, 0, 0),
                 "description": "Fixed score wrapping bug which affected scores above 65535. Guess why I never noticed this :)"
             });
             this.add(note);
             var note = new SiteNews();
             note.set({
-                "date": new Date(2016, 1, 12, 0, 0, 0),
+                "date": new Date(2016, 0, 12, 0, 0, 0),
                 "description": "Closing a watched recording now exits - you don't need to press Q"
             });
             this.add(note);
+
         }
     });
 
