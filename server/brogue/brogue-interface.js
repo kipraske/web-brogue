1cd9c36f68a5d52acc00773d5a9848b9e6a786c7
flend <flend@debian.debian>
2016-03-29 23:06:24 +0100

    Fixing bug in brogue interface

--- server/brogue/brogue-interface.js
+++ server/brogue/brogue-interface.js
@@ -429,7 +429,7 @@ BrogueInterface.prototype.attachChildEvents = function () {
 
         //err = 11
         //This occurs brogue has gone non-responsive and the input buffer is full
-        self.resetBrogueConnection();
+        self.resetBrogueConnection(self);
 
         self.brogueEvents.emit('quit', 'Error when writing to client socket - normally brogue has exited');
     });
