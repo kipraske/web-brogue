c2bc71a3693aceb7b809f8883a8757070de1fd01
flend <flend@debian.debian>
2016-04-23 10:45:56 +0100

    Adding delete operation

--- scripts/news/news.js
+++ scripts/news/news.js
@@ -14,6 +14,8 @@ var seqModel = require("./seq-model");
 
 if(argv._.length < 2) {
     console.log("Usage: save 'news-to-save'");
+    console.log("Usage: delete (seq-no)");
+    console.log("Usage: list all");
     process.exit(1);
 }
 
@@ -21,14 +23,14 @@ if(argv._.length < 2) {
 async.series([
     function(finished) {
         seqModel.find(function (err, seq) {
-            console.log("Found seq: " + JSON.stringify(seq));
+
             if (err) {
                 console.log("Error accessing sequences " + JSON.stringify(err));
                 process.exit(1);
             }
 
             if (seq.length == 0) {
-                console.log("Creating new sequence");
+                console.log("Creating sequence table");
                 seqModel.create({"_id": SEQ_NAME, "seq": 0 }, function (err) {
                     if (err) {
                         console.log("Error creating initial sequence " + JSON.stringify(err));
@@ -44,44 +46,80 @@ async.series([
     },
     function(finished) {
 
-        console.log("step2");
+        if(argv._[0] == "save") {
 
-        //This doesn't work and needs to be turned into a working command!
+            var query = {"_id": SEQ_NAME};
+            var update = {$inc: { seq: 1 }};
+            var options = {new: true};
 
-        var query = {"_id": SEQ_NAME};
-        var update = {$inc: { seq: 1 }};
-        var options = {new: true};
+            seqModel.findOneAndUpdate(query, update, options,
+                function (err, seq) {
 
-        seqModel.findOneAndUpdate(query, update, options,
-            function(err, seq) {
-
-                console.log("Found seq: " + JSON.stringify(seq));
-
-                if(err) {
-                    console.log("Error updating sequence " + JSON.stringify(err));
-                    process.exit(1);
-                }
-
-                var newsStory = {
-                    date: new Date(),
-                    story: argv._[1],
-                    seq: seq.seq
-                };
-
-                console.log(JSON.stringify(newsStory));
-
-                newsModel.create(newsStory, function (err) {
                     if (err) {
-                        console.log("Error saving story " + JSON.stringify(err));
+                        console.log("Error updating sequence " + JSON.stringify(err));
+                        process.exit(1);
+                    }
+
+                    var newsStory = {
+                        date: new Date(),
+                        story: argv._[1],
+                        seq: seq.seq
+                    };
+
+                    console.log(JSON.stringify(newsStory));
+
+                    newsModel.create(newsStory, function (err) {
+                        if (err) {
+                            console.log("Error saving story " + JSON.stringify(err));
+                        }
+                        else {
+                            console.log("Wrote story successfully");
+                        }
+
+                        finished();
+                    });
+                }
+            );
+        }
+        else if(argv._[0] == "delete") {
+
+            var query = {"seq": argv._[1]};
+
+            newsModel.findOneAndRemove(query,
+                function(err, news) {
+
+                    if (err) {
+                        console.log("Unable to delete entry: seq: " + news.seq + ": " + JSON.stringify(err));
+                        process.exit(1);
                     }
                     else {
-                        console.log("Wrote story successfully");
+                        console.log("Deleted story with id: " + news.seq);
                     }
 
                     finished();
                 });
-            }
-        );
+        }
+        else if(argv._[0] == "list") {
+
+            newsModel.find(query,
+                function(err, news) {
+
+                    if (err) {
+                        console.log("Unable to find records: " + JSON.stringify(err));
+                        process.exit(1);
+                    }
+                    else {
+
+                        console.log(news);
+                    }
+
+                    finished();
+                });
+        }
+        else {
+            console.log("Unknown command.");
+            finished();
+        }
     },
     function() {
         process.exit(0);
