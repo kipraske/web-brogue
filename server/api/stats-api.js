c3450547bbc0bab3f47aae4ceaa21fc2f44e8f4c
flend <flend@debian.debian>
2016-09-17 21:24:55 +0100

    Adding more causes and getting the API to work

--- server/api/stats-api.js
+++ server/api/stats-api.js
@@ -3,59 +3,92 @@ var _ = require("underscore");
 
 module.exports = function(app) {
 
-    var calculateLevelStats = function () {
-
-        console.log("Calculating level stats");
-
-        GameRecord.find({}).lean().exec(function (err, games) {
-
-            var monsterDeathGames = _.filter(games, function (game) {
-                return game.description && game.description.search("Killed") != -1;
-            });
-
-            var burnedDeathGames = _.filter(games, function (game) {
-                return game.description && game.description.search("Burned") != -1;
-            });
-
-            var monsterDeathGamesWithCause = _.map(monsterDeathGames, function (game) {
-
-                var monsterRe = new RegExp("by\\s+an?\\s+(.*?)\\s+on");
-                var descriptionMatch = monsterRe.exec(game.description);
-                if (descriptionMatch) {
-                    game["cause"] = descriptionMatch[1];
-                }
-
-                return game;
-            });
-
-            var burnedDeathGamesWithCause = _.map(burnedDeathGames, function(game) {
-                game["cause"] = "Burned to death";
-                return game;
-            });
-
-            var allDeathGamesWithCause = monsterDeathGamesWithCause.concat(burnedDeathGamesWithCause);
-
-            var deathGamesByLevel = _.groupBy(allDeathGamesWithCause, "level");
-
-            var deathNumbersByLevel = _.mapObject(deathGamesByLevel, function(levelGames, level) {
-
-                var deathsByCauseOnLevel = _.groupBy(levelGames, "cause");
-                var numberOfDeathsByCauseOnLevel = _.mapObject(deathsByCauseOnLevel, function(causeGames, thisCause) {
-                    return causeGames.length;
-                });
-
-                var numberOfDeathsByCauseOnLevelAsArray = _.map(numberOfDeathsByCauseOnLevel, function(value, key){
-                    return { cause : key, frequency : value };
-                });
-                var numberOfDeathsByCauseOnLevelAsArraySorted = _.sortBy(numberOfDeathsByCauseOnLevelAsArray, "frequency").reverse();
-
-                return numberOfDeathsByCauseOnLevelAsArraySorted;
-            });
-        });
-    };
-
     app.get("/api/stats/levels", function (req, res) {
 
-        return res;
+        res.format({
+            json: function () {
+                GameRecord.find({}).lean().exec(function (err, games) {
+
+                    var monsterDeathGames = _.filter(games, function (game) {
+                        return game.description && game.description.search("Killed") != -1;
+                    });
+
+                    var monsterDeathGamesWithCause = _.map(monsterDeathGames, function (game) {
+
+                        var monsterRe = new RegExp("by\\s+[an]*\\s+(.*?)\\s+on");
+                        var descriptionMatch = monsterRe.exec(game.description);
+                        if (descriptionMatch) {
+                            game["cause"] = descriptionMatch[1];
+                        }
+                        else {
+                            game["cause"] = "unknown";
+                        }
+
+                        return game;
+                    });
+
+                    var burnedDeathGames = _.filter(games, function (game) {
+                        return game.description && game.description.search("Burned") != -1;
+                    });
+
+                    var burnedDeathGamesWithCause = _.map(burnedDeathGames, function(game) {
+                        game["cause"] = "burned to death";
+                        return game;
+                    });
+
+                    var poisonedDeathGames = _.filter(games, function (game) {
+                        return game.description && game.description.search("poison") != -1;
+                    });
+
+                    var poisonedDeathGamesWithCause = _.map(poisonedDeathGames, function(game) {
+                        game["cause"] = "poison";
+                        return game;
+                    });
+
+                    var starvedDeathGames = _.filter(games, function (game) {
+                        return game.description && game.description.search("Starved") != -1;
+                    });
+
+                    var starvedDeathGamesWithCause = _.map(starvedDeathGames, function(game) {
+                        game["cause"] = "starved";
+                        return game;
+                    });
+
+                    var lavaDeathGames = _.filter(games, function (game) {
+                        return game.description && game.description.search("by lava") != -1;
+                    });
+
+                    var lavaDeathGamesWithCause = _.map(lavaDeathGames, function(game) {
+                        game["cause"] = "lava";
+                        return game;
+                    });
+
+                    var allDeathGamesWithCause = monsterDeathGamesWithCause
+                        .concat(burnedDeathGamesWithCause)
+                        .concat(poisonedDeathGamesWithCause)
+                        .concat(starvedDeathGamesWithCause)
+                        .concat(lavaDeathGamesWithCause);
+
+                    var deathGamesByLevel = _.groupBy(allDeathGamesWithCause, "level");
+
+                    var deathNumbersByLevel = _.mapObject(deathGamesByLevel, function(levelGames, level) {
+
+                        var deathsByCauseOnLevel = _.groupBy(levelGames, "cause");
+                        var numberOfDeathsByCauseOnLevel = _.mapObject(deathsByCauseOnLevel, function(causeGames, thisCause) {
+                            return causeGames.length;
+                        });
+
+                        var numberOfDeathsByCauseOnLevelAsArray = _.map(numberOfDeathsByCauseOnLevel, function(value, key){
+                            return { cause : key, frequency : value };
+                        });
+                        var numberOfDeathsByCauseOnLevelAsArraySorted = _.sortBy(numberOfDeathsByCauseOnLevelAsArray, "frequency").reverse();
+
+                        return numberOfDeathsByCauseOnLevelAsArraySorted;
+                    });
+
+                    res.json(deathNumbersByLevel);
+                });
+            }
+        });
     });
 };
--- server/teststats.js
+++ server/teststats.js
@@ -17,11 +17,23 @@ var calculateLevelStats = function () {
         });
 
         var noMonsterDeathGames = _.filter(games, function (game) {
-            return game.description && game.description.search("Quit") == -1 && game.description.search("Killed") == -1;
+            return game.description  && game.description.search("Starved") == -1 && game.description.search("poison") == -1 && game.description.search("Escaped") == -1 && game.description.search("Quit") == -1 && game.description.search("Burned") == -1 && game.description.search("Killed") == -1;
         });
 
         var burnedDeathGames = _.filter(games, function (game) {
             return game.description && game.description.search("Burned") != -1;
+        });
+
+        var poisonedDeathGames = _.filter(games, function (game) {
+            return game.description && game.description.search("poison") != -1;
+        });
+
+        var starvedDeathGames = _.filter(games, function (game) {
+            return game.description && game.description.search("Starved") != -1;
+        });
+
+        var lavaDeathGames = _.filter(games, function (game) {
+            return game.description && game.description.search("by lava") != -1;
         });
 
         console.log("Records with killed: " + monsterDeathGames.length);
@@ -29,25 +41,50 @@ var calculateLevelStats = function () {
         var monsterDeathGamesWithCause = _.map(monsterDeathGames, function (game) {
 
            //var monsterRe = new RegExp("by\s+an?\s+(.*?)\s+on");
-           var monsterRe = new RegExp("by\\s+an?\\s+(.*?)\\s+on");
+            var monsterRe = new RegExp("by\\s+[an]*\\s+(.*?)\\s+on");
            var descriptionMatch = monsterRe.exec(game.description);
             if (descriptionMatch) {
                 console.log("match");
+
+                if(!descriptionMatch[1]) {
+                    console.log("undefined" + descriptionMatch);
+                }
                 game["cause"] = descriptionMatch[1];
+
             }
             else {
-                console.log("no match");
+                console.log("no match: + " + descriptionMatch);
+                game["cause"] = "unknown";
             }
 
             return game;
         });
 
         var burnedDeathGamesWithCause = _.map(burnedDeathGames, function(game) {
-            game["cause"] = "Burned to death";
+            game["cause"] = "burned to death";
             return game;
         });
 
-        var allDeathGamesWithCause = monsterDeathGamesWithCause.concat(burnedDeathGamesWithCause);
+        var poisonedDeathGamesWithCause = _.map(poisonedDeathGames, function(game) {
+            game["cause"] = "poison";
+            return game;
+        });
+
+        var starvedDeathGamesWithCause = _.map(starvedDeathGames, function(game) {
+            game["cause"] = "starved";
+            return game;
+        });
+
+        var lavaDeathGamesWithCause = _.map(lavaDeathGames, function(game) {
+            game["cause"] = "lava";
+            return game;
+        });
+
+        var allDeathGamesWithCause = monsterDeathGamesWithCause
+            .concat(burnedDeathGamesWithCause)
+            .concat(poisonedDeathGamesWithCause)
+            .concat(starvedDeathGamesWithCause)
+            .concat(lavaDeathGamesWithCause);
 
         var deathGamesByLevel = _.groupBy(allDeathGamesWithCause, "level");
 
@@ -70,13 +107,16 @@ var calculateLevelStats = function () {
 
         console.log(JSON.stringify(deathNumbersByLevel, null, 2));
 
-/*
-        console.log("Killed Games");
-        console.log(JSON.stringify(monsterDeathGamesWithCause, null, 2));
 
+        //console.log("Killed Games");
+       // console.log(JSON.stringify(monsterDeathGamesWithCause, null, 2));
+/*
         console.log("Burned Games");
         console.log(JSON.stringify(burnedDeathGamesWithCause, null, 2));
 */
+
+    //    console.log("No monster Games");
+     //   console.log(JSON.stringify(noMonsterDeathGames, null, 2));
 
     });
 };
