32327a16d050b8b3beec7b797fa5f45c07ec2f05
flend <flend@debian.debian>
2016-10-06 08:04:20 +0100

    Breaking out death regexp function and adding more tests

--- server/api/stats-api.js
+++ server/api/stats-api.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var GameRecordSchema = require("../database/game-record-model");
 
 var brogueConstants = require('../brogue/brogue-constants.js');
+var stats = require('../stats/stats.js');
 var _ = require("underscore");
 
 var GameRecord = mongoose.model('GameRecord', GameRecordSchema);
@@ -19,65 +20,7 @@ module.exports = function(app) {
             json: function () {
                 GameRecord.find({}).lean().exec(function (err, games) {
 
-                    var monsterDeathGames = _.filter(games, function (game) {
-                        return game.description && game.description.search("Killed") != -1;
-                    });
-
-                    var monsterDeathGamesWithCause = _.map(monsterDeathGames, function (game) {
-
-                        var monsterRe = new RegExp("by\\s+[an]*\\s+(.*?)\\s+on");
-                        var descriptionMatch = monsterRe.exec(game.description);
-                        if (descriptionMatch) {
-                            game["cause"] = descriptionMatch[1];
-                        }
-                        else {
-                            game["cause"] = "unknown";
-                        }
-
-                        return game;
-                    });
-
-                    var burnedDeathGames = _.filter(games, function (game) {
-                        return game.description && game.description.search("Burned") != -1;
-                    });
-
-                    var burnedDeathGamesWithCause = _.map(burnedDeathGames, function(game) {
-                        game["cause"] = "burned to death";
-                        return game;
-                    });
-
-                    var poisonedDeathGames = _.filter(games, function (game) {
-                        return game.description && game.description.search("poison") != -1;
-                    });
-
-                    var poisonedDeathGamesWithCause = _.map(poisonedDeathGames, function(game) {
-                        game["cause"] = "poison";
-                        return game;
-                    });
-
-                    var starvedDeathGames = _.filter(games, function (game) {
-                        return game.description && game.description.search("Starved") != -1;
-                    });
-
-                    var starvedDeathGamesWithCause = _.map(starvedDeathGames, function(game) {
-                        game["cause"] = "starved";
-                        return game;
-                    });
-
-                    var lavaDeathGames = _.filter(games, function (game) {
-                        return game.description && game.description.search("by lava") != -1;
-                    });
-
-                    var lavaDeathGamesWithCause = _.map(lavaDeathGames, function(game) {
-                        game["cause"] = "lava";
-                        return game;
-                    });
-
-                    var allDeathGamesWithCause = monsterDeathGamesWithCause
-                        .concat(burnedDeathGamesWithCause)
-                        .concat(poisonedDeathGamesWithCause)
-                        .concat(starvedDeathGamesWithCause)
-                        .concat(lavaDeathGamesWithCause);
+                    var allDeathGamesWithCause = stats.deathGamesWithCauses(games);
 
                     var deathGamesByLevel = _.groupBy(allDeathGamesWithCause, "level");
 
--- server/test/stats-api-test.js
+++ server/test/stats-api-test.js
@@ -67,6 +67,137 @@ describe("stats/general", function(){
             });
     });
 });
+
+describe("stats/levels", function(){
+
+    beforeEach(function(done) {
+
+        var gameRecord1 = {
+            username: "flend",
+            date: new Date("2011-05-26T07:56:00.123Z"),
+            score: 100,
+            seed: 200,
+            level: 3,
+            result: brogueConstants.gameOver.GAMEOVER_DEATH,
+            easyMode: false,
+            description: "Killed by a pink jelly on depth 3.",
+            recording: "file1"
+        };
+
+        var gameRecord2 = {
+            username: "flend",
+            date: new Date("2011-06-26T07:56:00.123Z"),
+            score: 150,
+            seed: 250,
+            level: 5,
+            result: brogueConstants.gameOver.GAMEOVER_DEATH,
+            easyMode: false,
+            description: "Killed by a violent explosion on depth 5.",
+            recording: "file2"
+        };
+
+        var gameRecord3 = {
+            username: "ccc",
+            date: new Date("2011-06-27T07:56:00.123Z"),
+            score: 151,
+            seed: 251,
+            level: 1,
+            result: brogueConstants.gameOver.GAMEOVER_DEATH,
+            easyMode: false,
+            description: "Killed by a rat on depth 1.",
+            recording: "file3"
+        };
+
+        var gameRecord4 = {
+            username: "ccc2",
+            date: new Date("2012-06-27T07:56:00.123Z"),
+            score: 152,
+            seed: 252,
+            level: 1,
+            result: brogueConstants.gameOver.GAMEOVER_DEATH,
+            easyMode: false,
+            description: "Killed by a rat on depth 1.",
+            recording: "file4"
+        };
+
+        var gameRecord5 = {
+            username: "ccc3",
+            date: new Date("2013-06-27T07:56:00.123Z"),
+            score: 153,
+            seed: 253,
+            level: 1,
+            result: brogueConstants.gameOver.GAMEOVER_DEATH,
+            easyMode: false,
+            description: "Killed by a jackal on depth 1.",
+            recording: "file5"
+        };
+
+        db.model('GameRecord', gameRecordSchema).create([gameRecord1, gameRecord2, gameRecord3, gameRecord4, gameRecord5], function() {
+            done();
+        });
+    });
+
+    afterEach(function(done) {
+
+        //delete all the customer records
+        db.model('GameRecord', gameRecordSchema).remove({}, function() {
+            done();
+        });
+    });
+
+    it("returns status 200", function(done) {
+        request(server)
+            .get("/api/stats/levels")
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+    });
+
+    it("groups causes by deaths correctly", function(done) {
+        request(server)
+            .get("/api/stats/levels")
+            .set('Accept', 'application/json')
+            .end(function(err, res) {
+                var bodyObj = JSON.parse(res.text);
+
+                var level1TopDeath = bodyObj[0];
+                expect(level1TopDeath).to.have.property('level', 1);
+                expect(level1TopDeath).to.have.property('cause', 'rat');
+                expect(level1TopDeath).to.have.property('frequency', 2);
+                expect(level1TopDeath).to.have.property('rank', 1);
+
+                var level1SecoundDeath = bodyObj[1];
+                expect(level1SecoundDeath).to.have.property('level', 1);
+                expect(level1SecoundDeath).to.have.property('cause', 'jackal');
+                expect(level1SecoundDeath).to.have.property('frequency', 1);
+                expect(level1SecoundDeath).to.have.property('rank', 2);
+                done();
+            });
+    });
+
+    it("restricts records per level by maxCauses", function(done) {
+        request(server)
+            .get("/api/stats/levels?maxCauses=1")
+            .set('Accept', 'application/json')
+            .end(function(err, res) {
+                var bodyObj = JSON.parse(res.text);
+
+                var level1TopDeath = bodyObj[0];
+                expect(level1TopDeath).to.have.property('level', 1);
+                expect(level1TopDeath).to.have.property('cause', 'rat');
+                expect(level1TopDeath).to.have.property('frequency', 2);
+                expect(level1TopDeath).to.have.property('rank', 1);
+
+                var level3TopDeath = bodyObj[1];
+                expect(level3TopDeath).to.have.property('level', 3);
+                expect(level3TopDeath).to.have.property('cause', 'pink jelly');
+                expect(level3TopDeath).to.have.property('frequency', 1);
+                expect(level3TopDeath).to.have.property('rank', 1);
+                done();
+            });
+    });
+});
+
 /*
 describe("stats/general", function(){
 
